6ef97728ad3f2758a9e06c81169ad8340cf4b8c9
Hans W. Uhlig <hans.uhlig@ibm.com>
2017-12-12 17:33:26 -0800

    Modifications

--- source/cube.hpp
+++ source/cube.hpp
@@ -1,8 +1,10 @@
 #ifndef LEDCUBE_CUBE_HPP
 #define LEDCUBE_CUBE_HPP
+
 #include <wiringPi.h>
 #include <cassert>
 #include <cstdint>
+#include <vector>
 #include <array>
 
 const uint8_t shiftDelay = 15;
@@ -13,21 +15,30 @@ public:
     using Buffer = std::array<uint8_t, WIDTH * HEIGHT * DEPTH>;
 
     class Frame {
-        friend class MonochromeCube;
-
+        uint8_t m_duration;
         Buffer m_buffer;
     public:
-        unsigned int getWidth() const { return WIDTH; }
+        uint8_t getWidth() const { return WIDTH; }
 
-        unsigned int getHeight() const { return HEIGHT; }
+        uint8_t getHeight() const { return HEIGHT; }
 
-        unsigned int getDepth() const { return DEPTH; }
+        uint8_t getDepth() const { return DEPTH; }
 
-        Frame &set(uint8_t x, uint8_t y, uint8_t z, uint8_t brightness) {
-            assert(0 <= x && x < WIDTH && 0 <= y && y < HEIGHT && 0 <= z && z < DEPTH);
-            m_buffer[y * WIDTH * DEPTH + z * WIDTH + x] = brightness;
+        uint8_t getDuration() const { return m_duration; }
+
+        Frame &setDuration(uint8_t duration) {
+            m_duration = duration;
             return *this;
         }
+
+        Frame &clear() { m_buffer.fill(0); }
+
+        Frame &setPixel(uint8_t x, uint8_t y, uint8_t z, uint8_t luminosity) {
+            assert(0 <= x && x < WIDTH && 0 <= y && y < HEIGHT && 0 <= z && z < DEPTH);
+            m_buffer[y * WIDTH * DEPTH + z * WIDTH + x] = luminosity;
+            return *this;
+        }
+
 
         bool getState(uint8_t mask, uint8_t x, uint8_t y, uint8_t z) {
             assert(0 <= x && x < WIDTH && 0 <= y && y < HEIGHT && 0 <= z && z < DEPTH);
@@ -37,10 +48,51 @@ public:
         Frame() {}
 
         Frame(Buffer buffer) : m_buffer(buffer) {}
+
+        Frame(Buffer buffer, uint8_t duration) : m_buffer(buffer), m_duration(duration) {}
     };
 
-    MonochromeCube(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t masterResetPin, uint8_t outputEnablePin) :
-            m_dataPin(dataPin), m_clockPin(clockPin), m_latchPin(latchPin), m_masterResetPin(masterResetPin), m_outputEnablePin(outputEnablePin) {
+    class Animation {
+        /// Empty Frame
+        static const Frame emptyFrame = Frame(Buffer(), 1);
+
+        std::vector<Frame> m_frames;
+        std::vector<size_t> m_indices;
+        bool m_repeat;
+    public:
+        Animation();
+
+        Animation(std::vector<Frame> frames)
+                : m_frames(frames), m_repeat(false) {
+            for (int i = 0; i < frames.size(); i++) m_indices.push_back(i);
+        }
+
+        Animation(std::vector<Frame> frames, bool repeat)
+                : m_frames(frames), m_repeat(repeat) {
+            for (int i = 0; i < frames.size(); i++) m_indices.push_back(i);
+        }
+
+        Animation(std::vector<Frame> frames, std::vector<size_t> indices)
+                : m_frames(frames), m_indices(indices), m_repeat(false) {
+        }
+
+
+        Animation(std::vector<Frame> frames, std::vector<size_t> indices, bool repeat)
+                : m_frames(frames), m_indices(indices), m_repeat(repeat) {
+        }
+
+        const Frame &getFrame(size_t frameNumber) {
+            if (m_repeat) {
+                return m_frames[m_indices[frameNumber % m_indices.size()]];
+            } else if (frameNumber > m_indices.size()) {
+                return emptyFrame;
+            }
+        }
+    };
+
+    MonochromeCube(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t masterResetPin, uint8_t outputEnablePin)
+            : m_dataPin(dataPin), m_clockPin(clockPin), m_latchPin(latchPin), m_masterResetPin(masterResetPin),
+              m_outputEnablePin(outputEnablePin) {
         wiringPiSetupGpio();
         pinMode(dataPin, OUTPUT);
         pinMode(clockPin, OUTPUT);
@@ -50,7 +102,8 @@ public:
         digitalWrite(dataPin, LOW);
         digitalWrite(clockPin, LOW);
         digitalWrite(latchPin, LOW);
-        digitalWrite(masterResetPin, LOW); delay(shiftDelay);
+        digitalWrite(masterResetPin, LOW);
+        delay(shiftDelay);
         digitalWrite(masterResetPin, HIGH);
         digitalWrite(outputEnablePin, LOW);
     }
@@ -70,8 +123,12 @@ public:
         digitalWrite(m_masterResetPin, HIGH);
     }
 
-    void displayFrame(Frame frame, uint32_t duration) {
-        for (uint32_t t = duration; t > 0; t--) {
+    void displayAnimation(Animation animation) {
+
+    }
+
+    void displayFrame(Frame frame) {
+        for (uint8_t t = frame.getDuration(); t > 0; t--) {
             for (uint8_t seq = 0; seq < 8; seq++) {
                 const uint8_t mask = (1 << seq);
                 for (uint8_t y = 0; y < HEIGHT; y++) {
@@ -80,18 +137,21 @@ public:
                         for (uint8_t z = 0; z < DEPTH; z++) {
                             const bool bit = frame.getState(mask, x, y, z);
                             digitalWrite(m_dataPin, bit ? HIGH : LOW);
-                            digitalWrite(m_clockPin, HIGH); delayMicroseconds(shiftDelay);
+                            digitalWrite(m_clockPin, HIGH);
+                            delayMicroseconds(shiftDelay);
                             digitalWrite(m_clockPin, LOW);
                         }
                     }
                     for (uint8_t h = 0; h < HEIGHT; h++) {
                         const bool bit = (h == y ? HIGH : LOW);
                         digitalWrite(m_dataPin, bit ? HIGH : LOW);
-                        digitalWrite(m_clockPin, HIGH); delayMicroseconds(shiftDelay);
+                        digitalWrite(m_clockPin, HIGH);
+                        delayMicroseconds(shiftDelay);
                         digitalWrite(m_clockPin, LOW);
                         digitalWrite(m_dataPin, LOW);
                     }
-                    digitalWrite(m_latchPin, HIGH); delayMicroseconds(shiftDelay);
+                    digitalWrite(m_latchPin, HIGH);
+                    delayMicroseconds(shiftDelay);
                 }
             }
         }
--- source/test.cpp
+++ source/test.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "cube.hpp"
 #include "patterns.hpp"
 
@@ -7,16 +9,94 @@ const uint8_t PIN_OE = 19; // Output Enable
 const uint8_t PIN_LATCH = 13; // Latch
 const uint8_t PIN_CLOCK = 22; // Clock
 const uint8_t PIN_MR = 17; // Master Reset
-const uint32_t SPEED = 5; // Times to repeat each frame
+const uint32_t SPEED = 5; // Duration of each frame
+const uint8_t L0 = 0xFF, L1 = 0xAA, L2 = 0x88, L3 = 0x80;
 
 int main(int argc, char **argv) {
     std::cout << "Starting up" << std::endl;
     MonochromeCube<4, 4, 4> cube(PIN_DATA, PIN_CLOCK, PIN_LATCH, PIN_MR, PIN_OE);
-    std::vector<MonochromeCube<4, 4, 4>::Frame> *pattern = &pattern_1;
+
+    srand(time(NULL));
+
+    uint8_t x = rand() % 4, y = rand() % 4, z = rand() % 4;
+
+    struct Position {
+        uint8_t x, y, z;
+    };
+
+    Position trail[4] = {{x, y, z},
+                         {x, y, z},
+                         {x, y, z},
+                         {x, y, z}};
 
     while (true) {
+        MonochromeCube<4, 4, 4>::Frame frame;
+        frame.setDuration(SPEED);
+        bool success = false;
+        uint8_t x = trail[0].x, y = trail[0].y, z = trail[0].z;
+        while (!success) {
+            switch (rand() % 7) {
+                case 0: // +X
+                    if (x + 1 < 4) {
+                        x = x + 1;
+                        success = true;
+                    }
+                    break;
+                case 1: // -X
+                    if (x - 1 > 0) {
+                        x = x - 1;
+                        success = true;
+                    }
+                    break;
+                case 2: // +Y
+                    if (y + 1 < 4) {
+                        y = y + 1;
+                        success = true;
+                    }
+                    break;
+                case 3: // -Y
+                    if (y - 1 > 0) {
+                        y = y - 1;
+                        success = true;
+                    }
+                    break;
+                case 4: // +Z
+                    if (z + 1 < 4) {
+                        z = z + 1;
+                        success = true;
+                    }
+                    break;
+                case 5: // -Z
+                    if (z - 1 > 0) {
+                        z = z - 1;
+                        success = true;
+                    }
+                    break;
+                case 6: // No Movement
+                    success = true;
+                    break;
+            }
+        }
+        trail[3] = trail[2];
+        trail[2] = trail[1];
+        trail[1] = trail[0];
+        trail[0].x = x;
+        trail[0].y = y;
+        trail[0].z = z;
+        frame.clear();
+        frame.setPixel(trail[3].x, trail[3].y, trail[3].z, L3);
+        frame.setPixel(trail[2].x, trail[2].y, trail[2].z, L2);
+        frame.setPixel(trail[1].x, trail[1].y, trail[1].z, L1);
+        frame.setPixel(trail[0].x, trail[0].y, trail[0].z, L0);
+        cube.displayFrame(frame);
+    }
+
+    /*
+    std::vector<MonochromeCube<4, 4, 4>::Frame> *pattern = &pattern_1;
+    while (true) {
         for (int f = 0; f < pattern->size(); f++) {
-            cube.displayFrame((*pattern)[f], SPEED);
+            cube.displayFrame((*pattern)[f]);
         }
     }
+    */
 }
